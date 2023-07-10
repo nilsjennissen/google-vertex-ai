1f8e8c3b93acd83ce75b3691bc4f6d4b544795e9
nilsjenn <93837106+nilsjenn@users.noreply.github.com>
2023-07-10 12:51:09 +0200

    Project google-vertex-ai started

---
+++ main.py
@@ -0,0 +1,148 @@
+''' This is the main application file for the CODER application. It contains the main functions and the support
+    functions for the application. '''
+
+#%% ----------------------------- IMPORTS  -----------------------------------
+# import libraries
+import streamlit as st
+import pandas as pd
+import numpy as np
+import credentials
+import os
+from audio_recorder_streamlit import audio_recorder
+import openai
+from gtts import gTTS
+import io
+from IPython.display import Audio
+import time
+#import en_core_web_sm
+import spacy_streamlit
+from pydub import AudioSegment
+import matplotlib.pyplot as plt
+from audio_recorder_streamlit import audio_recorder
+from IPython.display import Audio
+from langchain.callbacks import get_openai_callback
+# Import the Speech-to-Text client library
+from google.cloud import speech
+
+from google.cloud.speech_v2 import SpeechClient
+from google.cloud.speech_v2.types import cloud_speech
+
+
+
+#%% ----------------------------- LANGCHAIN FUNCTIONS -----------------------------------
+script_dir = os.path.dirname(os.path.abspath(__file__))
+OPENAI_API_KEY = credentials.OPENAI_API_KEY
+
+
+# %% ----------------------------- AUDIO RECORDING STREAMLIT -----------------------------------
+def rec_streamlit():
+    """Record audio and return the audio bytes"""
+    audio_bytes = audio_recorder(energy_threshold=(-1.0, 1.0), pause_threshold=6.0, text="",
+                                 recording_color="#FF0000", neutral_color="#49DE49", icon_name="microphone", icon_size="3x")
+
+    return audio_bytes
+
+
+#%% ----------------------------- OPEN AI FUNCTIONS  -----------------------------------
+def get_transcript_whisper(file_path):
+    '''Get the transcript of the audio file'''
+    openai.api_key = OPENAI_API_KEY
+    with open(file_path, "rb") as file:
+        transcription = openai.Audio.transcribe("whisper-1", file, response_format="json")
+    transcribed_text = transcription["text"]
+
+    return transcribed_text
+
+
+#%% ----------------------------- GOOGLE CLOUD SPEECH V1  -----------------------------------
+def get_transcript_google(file_path):
+    """Transcribe the given audio file."""
+    client = speech.SpeechClient()
+
+    with open(file_path, "rb") as audio_file:
+        content = audio_file.read()
+
+    audio = speech.RecognitionAudio(content=content)
+    config = speech.RecognitionConfig(
+            encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+            sample_rate_hertz=48000,
+            language_code="en-US",
+            model="default",
+            audio_channel_count=1,
+            enable_word_confidence=True,
+            enable_word_time_offsets=True,
+    )
+
+    response = client.recognize(config=config, audio=audio)
+
+    return response
+
+
+
+def quickstart_v2(
+    project_id: str,
+    audio_file: str,
+) -> cloud_speech.RecognizeResponse:
+    """Transcribe an audio file."""
+    # Instantiates a client
+    client = SpeechClient()
+
+    # Reads a file as bytes
+    with open(audio_file, "rb") as f:
+        content = f.read()
+
+    config = cloud_speech.RecognitionConfig(
+        auto_decoding_config={}, language_codes=["en-US"], model="latest_long"
+    )
+
+    request = cloud_speech.RecognizeRequest(
+        recognizer=f"projects/{project_id}/locations/global/recognizers/_",
+        config=config,
+        content=content,
+    )
+
+    # Transcribes the audio into text
+    response = client.recognize(request=request)
+
+    for result in response.results:
+        print(f"Transcript: {result.alternatives[0].transcript}")
+
+    return response
+
+
+#%% ----------------------------- SPEAK FUNCTIONS -----------------------------------
+def speak_answer(answer, tts_enabled):
+    if not tts_enabled:
+        return
+
+    tts = gTTS(text=answer, lang="en")
+    with io.BytesIO() as f:
+        tts.write_to_fp(f)
+        f.seek(0)
+        audio = Audio(f.read(), autoplay=True)
+        st.write(audio)
+
+
+#%% ----------------------------- MAIN APPLICATION FUNCTIONS -----------------------------------
+def home():
+    # ------------------ SETTINGS ------------------
+    st.set_page_config(page_title="Home", layout="wide")
+    st.markdown("""<style>.reportview-container .main .block-container {max-width: 95%;}</style>""",
+                unsafe_allow_html=True)
+
+    # ------------------ HOME PAGE ------------------
+    st.title("MAIN FILE MULTIAGENT 🎙️📖🥷")
+    st.write("""Use the power of LLMs with LangChain and OpenAI to scan through your documents. Find information 
+    and insight's with lightning speed. 🚀 Create new content with the support of state of the art language models and 
+    and voice command your way through your documents. 🎙️""")
+    st.write("Let's start interacting with GPT-4!")
+
+    # ------------------ SIDE BAR SETTINGS ------------------
+    st.sidebar.subheader("Settings:")
+    tts_enabled = st.sidebar.checkbox("Enable Text-to-Speech", value=False)
+    ner_enabled = st.sidebar.checkbox("Enable NER in Response", value=False)
+
+
+# Run home function
+if __name__ == "__main__":
+    home()
---
+++ pages/vertex_researcher.py
@@ -0,0 +1,299 @@
+'''
+This Vertex AI Agent is a combination of the Google Vertex AI and the Langchain Agent for research purposes.
+'''
+
+#%% ---------------------------------------------  IMPORTS  ----------------------------------------------------------#
+import pandas as pd
+from datetime import datetime
+import streamlit as st
+import requests
+
+from langchain.embeddings import VertexAIEmbeddings
+from langchain.llms import VertexAI
+import vertexai
+from vertexai.preview.language_models import ChatModel
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains.question_answering import load_qa_chain
+from langchain.vectorstores import FAISS
+import tempfile
+import os
+from langchain.vectorstores import FAISS
+from langchain.document_loaders import TextLoader
+from main import rec_streamlit, speak_answer, get_transcript_whisper
+import time
+import en_core_web_sm
+import spacy_streamlit
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chains import RetrievalQA
+from pptx import Presentation
+from pptx.util import Inches
+from serpapi import GoogleSearch
+from credentials import serp_api_key
+from langchain.llms import OpenAI
+import pandas as pd
+from serpapi import GoogleSearch
+from urllib.parse import urlsplit, parse_qsl
+
+from credentials import serp_api_key
+
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+#%% ----------------------------------------  VERTEXAI PRELOADS -----------------------------------------------------#
+# Initialise the vertexai environment
+vertexai.init(project="ghc-016", location="us-central1")
+embeddings = VertexAIEmbeddings()
+
+# Initialise the vertexai environment
+vertexai.init(project="ghc-016", location="us-central1")
+
+# Initialise the chat model
+model = ChatModel.from_pretrained("chat-bison@001")
+chat = model.start_chat(examples=[])
+
+# --------------------  SETTINGS  -------------------- #
+st.set_page_config(page_title="Home", layout="wide")
+st.markdown("""<style>.reportview-container .main .block-container {max-width: 95%;}</style>""", unsafe_allow_html=True)
+
+# --------------------- HOME PAGE -------------------- #
+st.title("VERTEX AI Research Assistant")
+st.write("""Use the power of LLMs with GOOGLE VERTEX AI and LangChain to scan through your documents. Find information 
+from google scholar and insight's with lightning speed. 🚀 Create new content with the support of state of the art 
+language models and and voice command your way through your documents. 🎙️""")
+
+
+#%% ---------------------------------------  PREPROCESS DOCUMENTS ----------------------------------------------------#
+def organic_results(search_word, start_year, end_year, num_pages):
+    print("extracting organic results..")
+
+    params = {
+        "api_key": serp_api_key,          # https://serpapi.com/manage-api-key
+        "engine": "google_scholar",
+        "q": search_word,  # search query
+        "hl": "en",        # language
+        "as_ylo": start_year,  # from start_year
+        "as_yhi": end_year,  # to end_year
+        "start": "0"       # first page
+    }
+
+    search = GoogleSearch(params)
+
+    organic_results_data = []
+
+    page_count = 0
+
+    while page_count < num_pages:
+        results = search.get_dict()
+
+        print(f"Currently extracting page №{results['serpapi_pagination']['current']}..")
+
+        for result in results["organic_results"]:
+            position = result["position"]
+            title = result["title"]
+            publication_info_summary = result["publication_info"]["summary"]
+            result_id = result["result_id"]
+            link = result.get("link")
+            result_type = result.get("type")
+            snippet = result.get("snippet")
+
+            try:
+              file_title = result["resources"][0]["title"]
+            except: file_title = None
+
+            try:
+              file_link = result["resources"][0]["link"]
+            except: file_link = None
+
+            try:
+              file_format = result["resources"][0]["file_format"]
+            except: file_format = None
+
+            try:
+              cited_by_count = int(result["inline_links"]["cited_by"]["total"])
+            except: cited_by_count = None
+
+            cited_by_id = result.get("inline_links", {}).get("cited_by", {}).get("cites_id", {})
+            cited_by_link = result.get("inline_links", {}).get("cited_by", {}).get("link", {})
+
+            try:
+              total_versions = int(result["inline_links"]["versions"]["total"])
+            except: total_versions = None
+
+            all_versions_link = result.get("inline_links", {}).get("versions", {}).get("link", {})
+            all_versions_id = result.get("inline_links", {}).get("versions", {}).get("cluster_id", {})
+
+            organic_results_data.append({
+              "page_number": results["serpapi_pagination"]["current"],
+              "position": position + 1,
+              "result_type": result_type,
+              "title": title,
+              "link": link,
+              "result_id": result_id,
+              "publication_info_summary": publication_info_summary,
+              "snippet": snippet,
+              "cited_by_count": cited_by_count,
+              "cited_by_link": cited_by_link,
+              "cited_by_id": cited_by_id,
+              "total_versions": total_versions,
+              "all_versions_link": all_versions_link,
+              "all_versions_id": all_versions_id,
+              "file_format": file_format,
+              "file_title": file_title,
+              "file_link": file_link,
+            })
+
+        if "next" in results.get("serpapi_pagination", {}):
+            search.params_dict.update(dict(parse_qsl(urlsplit(results["serpapi_pagination"]["next"]).query)))
+        else:
+            break
+
+        page_count += 1
+
+    df = pd.DataFrame(organic_results_data)
+
+    # get current date and time
+    now = datetime.now()
+
+    # format as string
+    dt_string = now.strftime("%Y-%m-%d_%H-%M-%S")
+
+    # save to csv
+    df.to_csv(f'papers/scholar_results_{dt_string}.csv', index=False)
+
+    return df
+
+#%% ---------------------------------------  PREPROCESS DOCUMENTS ----------------------------------------------------#
+def download_file(url):
+    # Send a HTTP request to the URL of the file, stream = True means that the file's content will be streamed when accessing the content attribute
+    response = requests.get(url, stream=True)
+
+    # Get the file name by splitting the URL at '/' and taking the last element
+    file_name = url.split('/')[-1]
+
+    # Add the .pdf extension to the file name
+    file_name = f"{file_name}.pdf"
+
+    st.write(f"Downloading file {file_name}..")
+
+    # Open the file and write the content into the file
+    with open(os.path.join('papers/', file_name), 'wb') as file:
+        for chunk in response.iter_content(chunk_size=1024):
+            file.write(chunk)
+
+
+#%% ---------------------------------------  PREPROCESS DOCUMENTS ----------------------------------------------------#
+# Check if 'selected_papers' and 'selected_papers_urls' are not in the session state
+if 'selected_papers' not in st.session_state:
+    st.session_state.selected_papers = []
+if 'selected_papers_urls' not in st.session_state:
+    st.session_state.selected_papers_urls = []
+
+with st.form(key='search_form'):
+    # Settings for Google Scholar search
+    start_year = st.number_input("Enter start year", 2000, 2023, 2022)
+    end_year = st.number_input("Enter end year", 2000, 2023, 2023)
+    num_pages = st.number_input("Enter number of pages - caution: many pages require more calls from serpapi", 1, 10, 1)
+    search_words = st.text_input("Enter google scholar search words", "artificial intelligence")
+
+    search_button = st.form_submit_button(label='Search for a topic')
+
+if search_button:
+    df = organic_results(search_words, start_year, end_year, num_pages)
+    # Save the DataFrame in the session state
+    st.session_state.df = df
+    st.dataframe(df)
+
+
+with st.form(key='select_form'):
+    # Check if 'df' exists in the session state
+    if 'df' in st.session_state:
+        # Use the DataFrame from the session state
+        df = st.session_state.df
+
+        # Filter the DataFrame to only include rows with PDF links
+        df_with_pdfs = df[df['file_link'].notna()]
+
+        # Store the selected papers and their URLs in the session state
+        selected_papers = st.multiselect('Select papers', df_with_pdfs['title'].tolist(), key='selected_papers')
+        st.session_state.selected_papers_urls = [df_with_pdfs[df_with_pdfs['title'] == paper]['file_link'].values[0] for
+                                                 paper in selected_papers]
+
+    select_button = st.form_submit_button(label='Select the files')
+
+# Update the session state with the selected papers
+if select_button:
+    st.write('Selected papers:', selected_papers)
+
+download_button = st.button(label='Download papers')
+
+# Check if the download button is pressed
+if download_button:
+    # Check if 'selected_papers' and 'selected_papers_urls' exist in the session state
+    if 'selected_papers' in st.session_state and 'selected_papers_urls' in st.session_state:
+        # Download the selected papers
+        for paper, url in zip(st.session_state.selected_papers, st.session_state.selected_papers_urls):
+            download_file(url)
+
+
+# ----------------- SIDE BAR SETTINGS ---------------- #
+st.sidebar.subheader("Settings:")
+tts_enabled = st.sidebar.checkbox("Enable Text-to-Speech", value=False)
+
+# ------------------ FILE UPLOADER ------------------- #
+st.sidebar.subheader("File Uploader:")
+uploaded_files = st.sidebar.file_uploader("Choose files", type=["csv", "html", "css", "py", "pdf", "ipynb"],
+                                          accept_multiple_files=True)
+st.sidebar.metric("Number of files uploaded", len(uploaded_files))
+st.sidebar.color_picker("Pick a color for the answer space", "#C14531")
+
+# Initialize docsearch as None
+docsearch = None
+
+# --------------------- USER INPUT --------------------- #
+user_input = st.text_area("")
+# If record button is pressed, rec_streamlit records and the output is saved
+audio_bytes = rec_streamlit()
+
+
+# ------------------- TRANSCRIPTION -------------------- #
+if audio_bytes or user_input:
+
+    if audio_bytes:
+        try:
+            with open("audio.wav", "wb") as file:
+                file.write(audio_bytes)
+        except Exception as e:
+            st.write("Error recording audio:", e)
+        transcript = get_transcript_whisper("audio.wav")
+    else:
+        transcript = user_input
+
+    st.write("**Recognized:**")
+    st.write(transcript)
+
+    if any(word in transcript for word in ["abort recording"]):
+        st.write("... Script stopped by user")
+        exit()
+
+    # ----------------------- ANSWER ----------------------- #
+    with st.spinner("Fetching answer ..."):
+        time.sleep(6)
+
+        llm = VertexAI()
+
+        # Text Splitter
+        text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=50)
+        #chunks = text_splitter.split_text(text)
+        #knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+        # Use the PDF agent to answer the question
+        #docs = knowledge_base.similarity_search(transcript)
+        # Show the amount of chunks found
+        #st.write(f"Found {len(docs)} chunks.")
+
+        #chain = load_qa_chain(llm, chain_type="stuff")
+        #answer = chain.run(input_documents=docs, question=transcript)
+        #st.write("**AI Response:**", answer)
+        #speak_answer(answer, tts_enabled)
+        #st.success("**Interaction finished**")
+
